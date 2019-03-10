287166ae557fd648e70594f0cbc6aa71c7309446
Martin Chovanec <chovanecm@gmail.com>
2019-03-10 15:31:34 +0100

    Fix autocomplete

--- include/autocomplete.sh
+++ include/autocomplete.sh
@@ -2,6 +2,6 @@ function autocomplete {
     local cur
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[@]::$(( ${#COMP_WORDS[@]} - 1 ))}"
-    COMPREPLY=($(compgen -W "$(SNOW_AUTOCOMPLETE=1 $prev)" -- ${cur}))
+    COMPREPLY=($(compgen -W "$(SNOW_AUTOCOMPLETE=1 $prev)" -- "${cur}"))
 }
 complete -F autocomplete snow
