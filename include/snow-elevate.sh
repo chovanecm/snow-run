fb1aa91ffbf8588ec22480e55373d29f1b01be8f
Martin Chovanec <chovanecm@gmail.com>
2019-03-12 10:31:32 +0100

    Remove unnecessary curl options for elevating roles

--- include/snow-elevate.sh
+++ include/snow-elevate.sh
@@ -12,5 +12,5 @@ then
    exit
 fi;
 
-curl "https://$snow_instance/api/now/ui/impersonate/role" -H 'Origin: https://$snow_instance' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'X-WantSessionNotificationMessages: true' -H "X-UserToken: $token" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'Connection: keep-alive' --data-binary '{"roles":"security_admin"}' -b $SNOW_COOKIE_FILE --cookie-jar $SNOW_COOKIE_FILE --compressed -v
+curl "https://$snow_instance/api/now/ui/impersonate/role" -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'X-WantSessionNotificationMessages: true' -H "X-UserToken: $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'Connection: keep-alive' --data-binary '{"roles":"security_admin"}' -b $SNOW_COOKIE_FILE --cookie-jar $SNOW_COOKIE_FILE --compressed -sS
 
