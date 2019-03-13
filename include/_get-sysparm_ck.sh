de6cc4b335f09780d7a2531316797c306f36fde8
Martin Chovanec <chovanecm@gmail.com>
2019-03-13 09:38:13 +0100

    add comments snow login, add -sS to curl

--- include/_get-sysparm_ck.sh
+++ include/_get-sysparm_ck.sh
@@ -1,1 +1,1 @@
-curl https://$snow_instance/login.do --cookie-jar $SNOW_COOKIE_FILE | $SNOW_INCLUDE_DIR/_grep-sysparm_ck.sh
+curl https://$snow_instance/login.do --cookie-jar $SNOW_COOKIE_FILE -sS | $SNOW_INCLUDE_DIR/_grep-sysparm_ck.sh
